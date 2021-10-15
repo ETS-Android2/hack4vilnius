2c66a85dc5519f8277062152b849afed3f471c8b
Spockas <dziugassimaitis@gmail.com>
2021-10-15 22:28:47 +0300

    added something

--- backend/underflow/api_api/serializers.py
+++ backend/underflow/api_api/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
-from models import HeapUser
+from .models import HeapUser
 
 
 class HeapUSerSerializer(serializers.ModelSerializer):
     user_email = serializers.CharField(max_length=255)
     user_password = serializers.CharField(max_length=255)
-    user_ID = serializers.IntegerField(primary_key=True)
+    user_ID = serializers.IntegerField()
 
     class Meta:
         model = HeapUser
--- backend/underflow/api_api/views.py
+++ backend/underflow/api_api/views.py
@@ -1,10 +1,21 @@
-from models import HeapUser
-from rest_framework.renderers import Te
+from django.http.response import HttpResponse
+from .models import HeapUser
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
-# class ProfileList(APIView):
-#     renderer_classes = [TemplateHTMLRenderer]
-#     template_name = ''
+from rest_framework import status
+from .serializers import HeapUSerSerializer
 
 # Create your views here.
+
+class HeapUserViews(APIView):
+    def post(self, request):
+        # serializer = HeapUSerSerializer(data=request.data)
+        return "Success"
+        # if serializer.is_valid():
+        #     serializer.save()
+        #     return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+        # else:
+        #     return Response({"status": "error", "errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+    
+    def get(self, request):
+        return HttpResponse("Success")
