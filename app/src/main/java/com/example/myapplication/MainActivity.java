428cbc49f5e157be0f7073e1ad4efc52ea2aeb9a
KorNito <kornelijuspacevicius@gmail.com>
2021-10-15 20:53:52 +0300

    Login screen prototype

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onSignIn(View view) {
+        EditText userName = findViewById(R.id.editTextUsername);
+        EditText password = findViewById(R.id.editTextPassword);
     }
 }
