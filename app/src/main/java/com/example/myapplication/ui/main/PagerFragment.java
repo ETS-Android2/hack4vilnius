4d8b8600bfe4613a181321d0cc9d35bcd9654395
RagonR <rapolasrusakevicius@gmail.com>
2021-10-17 02:38:02 +0300

    Scoreboard in progress

--- app/src/main/java/com/example/myapplication/ui/main/PagerFragment.java
+++ app/src/main/java/com/example/myapplication/ui/main/PagerFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
@@ -33,8 +34,9 @@ public class PagerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_scoreboard, container, false);
-        TextView textView = view.findViewById(R.id.text1);
-        textView.setText("Fragment #" + mPage);
+        //TODO
+        ListView userListView = view.findViewById(R.id.UserListView);
+
         return view;
     }
 }
