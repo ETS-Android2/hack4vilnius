2fda79abfe77e604e367367701b8c7beb1c61953
Nedas Matulevicius <nedas.matulevicius@student.manchester.ac.uk>
2021-10-17 01:18:32 +0300

    Prize (coupon) functionality hardcode

--- app/src/main/java/com/example/myapplication/UserDashboardActivity.java
+++ app/src/main/java/com/example/myapplication/UserDashboardActivity.java
@@ -162,12 +162,12 @@ public class UserDashboardActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    //    public void onCouponButtonClick (View view){
-//        Intent intent = new Intent(UserDashboardActivity.this, CouponsActivity.class);
-//        TextView userName = findViewById(R.id.welcomeText);
-//        intent.putExtra("username", userName.getText().toString());
-//        startActivity(intent);
-//    }
+    public void onCouponButtonClick (View view){
+        Intent intent = new Intent(UserDashboardActivity.this, PrizeActivity.class);
+        TextView userName = findViewById(R.id.welcomeText);
+        intent.putExtra("username", userName.getText().toString());
+        startActivity(intent);
+    }
     public void onMapButtonClick(View view) {
         Intent intent = new Intent(UserDashboardActivity.this, MapActivity.class);
         TextView userName = findViewById(R.id.welcomeText);
