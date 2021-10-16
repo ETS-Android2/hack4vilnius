ce58efbb460d2cf3cd840fc23b5af1e005bf1510
RagonR <rapolasrusakevicius@gmail.com>
2021-10-17 02:17:48 +0300

    Fully working registration

--- app/src/main/java/com/example/myapplication/Services/UserService.java
+++ app/src/main/java/com/example/myapplication/Services/UserService.java
@@ -15,7 +15,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class UserService {
-    public static final String QUERY_FOR_RESERVATION = "http://78.60.209.53:8000/heap/login";
+    public static final String QUERY_FOR_RESERVATION = "http://78.60.209.53:8000/heap/";
     Context context;
 
     public UserService(Context context) {
@@ -28,16 +28,37 @@ public class UserService {
         void onResponse(JSONObject user_object);
     }
 
-    public interface resultInterface {
-        void onResponse(JSONObject response);
-
-        void onErrorResponse(VolleyError error);
-    }
-
     public void Login(VolleyResponseListener volleyResponseListener, JSONObject user_object) {
         String url = QUERY_FOR_RESERVATION;
         JsonObjectRequest jsonObjReq = new JsonObjectRequest(
-                Request.Method.POST, url, user_object,
+                Request.Method.POST, url + "login", user_object,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.e("It WORKED", response.toString());
+                        volleyResponseListener.onResponse(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                volleyResponseListener.onError(error.getMessage());
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json; charset=utf-8");
+                return headers;
+            }
+
+        };
+
+        MySingleton.getInstance(context).addToRequestQueue(jsonObjReq);
+    }
+    public void Registration(VolleyResponseListener volleyResponseListener, JSONObject user_object) {
+        String url = QUERY_FOR_RESERVATION;
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(
+                Request.Method.POST, url + "registration", user_object,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
