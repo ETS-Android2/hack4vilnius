0a391417198f5a2c9cb30318337ab7c0899777eb
Nedas Matulevicius <nedas.matulevicius@student.manchester.ac.uk>
2021-10-16 16:16:57 +0300

    Username login check + visuals for QR code popup

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +24,14 @@ public class MainActivity extends AppCompatActivity {
     public void onSignIn(View view) {
         EditText userName = findViewById(R.id.editTextUsername);
         EditText password = findViewById(R.id.editTextPassword);
+
+        Context ctx = getApplicationContext();
+
+        if(TextUtils.isEmpty(userName.getText().toString())) {
+            Toast toast = Toast.makeText(ctx, "Please enter your username!", Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
 
         Intent intent = new Intent(MainActivity.this, UserDashboardActivity.class);
         intent.putExtra("username", userName.getText().toString());
